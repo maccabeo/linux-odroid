fb91f86f72d0793ea01b0ed83828c70690af717b
Alban Browaeys <prahal@yahoo.com>
2014-02-06 19:54:46 +0100

    Revert "BLOB: revert to hardkernel drm/mali"
    This reverts commit 8bdadcc55ab02f7099342169d474dee55326ab96.

--- drivers/gpu/drm/mali/mali_drv.c
+++ drivers/gpu/drm/mali/mali_drv.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010, 2012 ARM Limited. All rights reserved.
+ * Copyright (C) 2010, 2012-2013 ARM Limited. All rights reserved.
  * 
  * This program is free software and is provided to you under the terms of the GNU General Public License version 2
  * as published by the Free Software Foundation, and any use by you of this program is subject to the terms of such GNU licence.
@@ -8,36 +8,39 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-
-#include "mali_drm.h"
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/version.h>
+#include <drm/drmP.h>
+#include <drm/mali_drm.h>
 #include "mali_drv.h"
 
 static struct platform_device *pdev;
-
 static int mali_platform_drm_probe(struct platform_device *dev)
 {
-	pr_info("DRM: mali_platform_drm_probe()\n");
-	return mali_drm_init(dev);
+        pr_info("DRM: mali_platform_drm_probe()\n");
+        return mali_drm_init(dev);
 }
 
 static int mali_platform_drm_remove(struct platform_device *dev)
 {
-	pr_info("DRM: mali_platform_drm_remove()\n");
-	mali_drm_exit(dev);
-	return 0;
+        pr_info("DRM: mali_platform_drm_remove()\n");
+        mali_drm_exit(dev);
+        return 0;
 }
 
 static int mali_platform_drm_suspend(struct platform_device *dev, pm_message_t state)
 {
-	pr_info("DRM: mali_platform_drm_suspend()\n");
-	return 0;
+        pr_info("DRM: mali_platform_drm_suspend()\n");
+        return 0;
 }
 
 static int mali_platform_drm_resume(struct platform_device *dev)
 {
-	pr_info("DRM: mali_platform_drm_resume()\n");
-	return 0;
+        pr_info("DRM: mali_platform_drm_resume()\n");
+        return 0;
 }
+
 
 static char mali_drm_device_name[] = "mali_drm";
 static struct platform_driver platform_drm_driver = {
@@ -48,25 +51,26 @@ static struct platform_driver platform_drm_driver = {
 	.driver = {
 		.name = mali_drm_device_name,
 		.owner = THIS_MODULE,
-	}
+	},
 };
 
-static int mali_driver_load(struct drm_device *dev, unsigned long flags)
+#if 0
+static const struct drm_device_id dock_device_ids[] = {
+	{"MALIDRM", 0},
+	{"", 0},
+};
+#endif
+
+static int mali_driver_load(struct drm_device *dev, unsigned long chipset)
 {
-	int ret;
-	//unsigned long base, size;
+	#if 0
+	unsigned long base, size;
+	#endif
 	drm_mali_private_t *dev_priv;
-	pr_info("DRM: mali_driver_load()\n");
+	printk(KERN_ERR "DRM: mali_driver_load start\n");
 
 	dev_priv = kmalloc(sizeof(drm_mali_private_t), GFP_KERNEL);
-	if (dev_priv != NULL)
-		memset((void *)dev_priv, 0, sizeof(drm_mali_private_t));
-
-	if (dev_priv == NULL)
-	{
-		pr_info("DRM: No memory!\n");
-		return -ENOMEM;
-	}
+	if ( dev_priv == NULL ) return -ENOMEM;
 
 	dev->dev_private = (void *)dev_priv;
 
@@ -80,21 +84,24 @@ static int mali_driver_load(struct drm_device *dev, unsigned long flags)
 	base = drm_get_resource_start(dev, 1 );
 	size = drm_get_resource_len(dev, 1 );
 	#endif
-	ret = drm_sman_init(&dev_priv->sman, 2, 12, 8);
-	//if ( ret ) drm_free(dev_priv, sizeof(dev_priv), DRM_MEM_DRIVER);
-	if ( ret )
-		kfree( dev_priv );
+	idr_init(&dev_priv->object_idr);
 
-	return ret;
+	printk(KERN_ERR "DRM: mali_driver_load done\n");
+
+	return 0;
 }
 
 static int mali_driver_unload( struct drm_device *dev )
 {
 	drm_mali_private_t *dev_priv = dev->dev_private;
+	printk(KERN_ERR "DRM: mali_driver_unload start\n");
 
-	drm_sman_takedown(&dev_priv->sman);
-	//drm_free(dev_priv, sizeof(*dev_priv), DRM_MEM_DRIVER);
-	kfree( dev_priv );
+	idr_destroy(&dev_priv->object_idr);
+
+	kfree(dev_priv);
+	//kfree( dev_priv );
+	printk(KERN_ERR "DRM: mali_driver_unload done\n");
+
 	return 0;
 }
 
@@ -105,7 +112,35 @@ static const struct file_operations mali_driver_fops = {
         .poll = drm_poll,
         .unlocked_ioctl = drm_ioctl,
         .release = drm_release,
+#ifdef CONFIG_COMPAT
+	.compat_ioctl = drm_compat_ioctl,
+#endif
 };
+
+static int mali_driver_open(struct drm_device *dev, struct drm_file *file)
+{
+	struct mali_file_private *file_priv;
+
+	DRM_DEBUG_DRIVER("\n");
+	file_priv = kmalloc(sizeof(*file_priv), GFP_KERNEL);
+	if (!file_priv)
+		return -ENOMEM;
+
+	file->driver_priv = file_priv;
+
+	INIT_LIST_HEAD(&file_priv->obj_list);
+
+	return 0;
+}
+
+void mali_driver_postclose(struct drm_device *dev, struct drm_file *file)
+{
+	struct via_file_private *file_priv = file->driver_priv;
+
+	kfree(file_priv);
+}
+
+
 
 static struct drm_driver driver = 
 {
@@ -113,6 +148,9 @@ static struct drm_driver driver =
 	.load = mali_driver_load,
 	.unload = mali_driver_unload,
 	.context_dtor = NULL,
+	.open = mali_driver_open,
+	.preclose = mali_reclaim_buffers_locked,
+	.postclose = mali_driver_postclose,
 	.dma_quiescent = mali_idle,
 	.lastclose = mali_lastclose,
 	.ioctls = mali_ioctls,
@@ -127,36 +165,34 @@ static struct drm_driver driver =
 
 int mali_drm_init(struct platform_device *dev)
 {
-	pr_info("mali_drm_init(), driver name: %s, version %d.%d\n", DRIVER_NAME, DRIVER_MAJOR, DRIVER_MINOR);
-	driver.num_ioctls = mali_max_ioctl;
-	driver.kdriver.platform_device = dev;
-	return drm_platform_init(&driver, dev);
+        pr_info("mali_drm_init(), driver name: %s, version %d.%d\n", DRIVER_NAME, DRIVER_MAJOR, DRIVER_MINOR);
+        driver.num_ioctls = mali_max_ioctl;
+        driver.kdriver.platform_device = dev;
+        return drm_platform_init(&driver, dev);
 }
 
 void mali_drm_exit(struct platform_device *dev)
 {
-	drm_platform_exit(&driver, dev);
+        drm_platform_exit(&driver, dev);
 }
 
 static int __init mali_platform_drm_init(void)
 {
-	pdev = platform_device_register_simple(mali_drm_device_name, 0, NULL, 0);
-	return platform_driver_register(&platform_drm_driver);
+        pdev = platform_device_register_simple(mali_drm_device_name, 0, NULL, 0);
+        return platform_driver_register(&platform_drm_driver);
 }
 
 static void __exit mali_platform_drm_exit(void)
 {
-	platform_driver_unregister(&platform_drm_driver);
-	platform_device_unregister(pdev);
+        platform_device_unregister(pdev);
+        platform_driver_unregister(&platform_drm_driver);
 }
 
-#ifdef MODULE
 module_init(mali_platform_drm_init);
-#else
-late_initcall(mali_platform_drm_init);
-#endif
 module_exit(mali_platform_drm_exit);
 
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
 MODULE_AUTHOR("ARM Ltd.");
 MODULE_DESCRIPTION(DRIVER_DESC);
 MODULE_LICENSE("GPL and additional rights");
--- drivers/gpu/drm/mali/mali_mm.c
+++ drivers/gpu/drm/mali/mali_mm.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010, 2012 ARM Limited. All rights reserved.
+ * Copyright (C) 2010, 2012-2013 ARM Limited. All rights reserved.
  * 
  * This program is free software and is provided to you under the terms of the GNU General Public License version 2
  * as published by the Free Software Foundation, and any use by you of this program is subject to the terms of such GNU licence.
@@ -8,7 +8,8 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#include "mali_drm.h"
+#include <drm/drmP.h>
+#include <drm/mali_drm.h>
 #include "mali_drv.h"
 
 #define VIDEO_TYPE 0
@@ -17,105 +18,100 @@
 #define MALI_MM_ALIGN_SHIFT 4
 #define MALI_MM_ALIGN_MASK ( (1 << MALI_MM_ALIGN_SHIFT) - 1)
 
-
-static void *mali_sman_mm_allocate( void *private, unsigned long size, unsigned alignment )
-{
-	pr_err( "DRM: %s\n", __func__);
-	//struct mali_memreq req;
-
-	//req.size = size;
-	/* XXX _ump_ukk_allocate( _ump_uk_allocate_s *user_interaction ); */
-	//if (req.size == 0) return NULL;
-	//else return (void *)(unsigned long)~req.offset;
-	return NULL;
-}
-
-static void mali_sman_mm_free( void *private, void *ref )
-{
-	pr_err( "DRM: %s\n", __func__);
-	/*ump_free(~((unsigned long)ref));*/
-
-}
-
-static void mali_sman_mm_destroy( void *private )
-{
-	pr_err( "DRM: %s\n", __func__);
-}
-
-static unsigned long mali_sman_mm_offset( void *private, void *ref )
-{
-	pr_err( "DRM: %s\n", __func__);
-	return ~((unsigned long)ref);
-}
+struct mali_memblock {
+	struct drm_mm_node mm_node;
+	struct list_head owner_list;
+};
 
 static int mali_fb_init( struct drm_device *dev, void *data, struct drm_file *file_priv )
 {
 	drm_mali_private_t *dev_priv = dev->dev_private;
 	drm_mali_fb_t *fb = data;
-	int ret;
-	pr_err( "DRM: %s\n", __func__);
+	printk(KERN_ERR "DRM: %s\n", __func__);
 
 	mutex_lock(&dev->struct_mutex);
-	{
-		struct drm_sman_mm sman_mm;
-		sman_mm.private = (void *)0xFFFFFFFF;
-		sman_mm.allocate = mali_sman_mm_allocate;
-		sman_mm.free = mali_sman_mm_free;
-		sman_mm.destroy = mali_sman_mm_destroy;
-		sman_mm.offset = mali_sman_mm_offset;
-		ret = drm_sman_set_manager(&dev_priv->sman, VIDEO_TYPE, &sman_mm);
-	}
-
-	if (ret) {
-		DRM_ERROR("VRAM memory manager initialisation error\n");
-		mutex_unlock(&dev->struct_mutex);
-		return ret;
-	}
+	drm_mm_init(&dev_priv->vram_mm, 0, fb->size >> MALI_MM_ALIGN_SHIFT);
 
 	dev_priv->vram_initialized = 1;
 	dev_priv->vram_offset = fb->offset;
 
 	mutex_unlock(&dev->struct_mutex);
-	DRM_DEBUG("offset = %u, size = %u\n", fb->offset, fb->size);
+	DRM_DEBUG("offset = %lu, size = %lu\n", fb->offset, fb->size);
 
 	return 0;
 }
 
-static int mali_drm_alloc(struct drm_device *dev, struct drm_file *file_priv, void *data, int pool)
+static int mali_drm_alloc(struct drm_device *dev, struct drm_file *file, void *data, int pool)
 {
 	drm_mali_private_t *dev_priv = dev->dev_private;
 	drm_mali_mem_t *mem = data;
-	int retval = 0;
-	struct drm_memblock_item *item;
-	pr_err( "DRM: %s\n", __func__);
+	int retval = 0, user_key;
+	struct mali_memblock *item;
+	unsigned long offset;
+	struct mali_file_private *file_priv = file->driver_priv;
+	printk(KERN_ERR "DRM: %s\n", __func__);
 
+	if (pool > MEM_TYPE) {
+		DRM_ERROR("Unknown memory type allocation\n");
+		return -EINVAL;
+	}
 	mutex_lock(&dev->struct_mutex);
 
-	if (0 == dev_priv->vram_initialized )
-	{
+	if (0 == ((pool == VIDEO_TYPE) ? dev_priv->vram_initialized :
+			dev_priv->mem_initialized)) {
 		DRM_ERROR("Attempt to allocate from uninitialized memory manager.\n");
 		mutex_unlock(&dev->struct_mutex);
 		return -EINVAL;
 	}
 
-	mem->size = (mem->size + MALI_MM_ALIGN_MASK) >> MALI_MM_ALIGN_SHIFT;
-	item = drm_sman_alloc(&dev_priv->sman, pool, mem->size, 0,
-			      (unsigned long)file_priv);
-
-	mutex_unlock(&dev->struct_mutex);
-	if (item) 
-	{
-		mem->offset = dev_priv->vram_offset + (item->mm->offset(item->mm, item->mm_info) << MALI_MM_ALIGN_SHIFT);
-		mem->free = item->user_hash.key;
-		mem->size = mem->size << MALI_MM_ALIGN_SHIFT;
-	} else {
-		mem->offset = 0;
-		mem->size = 0;
-		mem->free = 0;
+	item = kzalloc(sizeof(*item), GFP_KERNEL);
+	if (!item) {
 		retval = -ENOMEM;
+		goto fail_alloc;
 	}
 
-	DRM_DEBUG("alloc %d, size = %d, offset = %d\n", pool, mem->size, mem->offset);
+	mem->size = (mem->size + MALI_MM_ALIGN_MASK) >> MALI_MM_ALIGN_SHIFT;
+	if (pool == MEM_TYPE) {
+		retval = drm_mm_insert_node(&dev_priv->mem_mm,
+					   &item->mm_node,
+					   mem->size, 0,
+					   DRM_MM_SEARCH_DEFAULT);
+		offset = item->mm_node.start;
+	} else {
+		retval = drm_mm_insert_node(&dev_priv->vram_mm,
+					   &item->mm_node,
+					   mem->size, 0,
+					   DRM_MM_SEARCH_DEFAULT);
+		offset = item->mm_node.start;
+	}
+	if (retval)
+		goto fail_alloc;
+
+	retval = idr_alloc(&dev_priv->object_idr, item, 1, 0, GFP_KERNEL);
+	if (retval < 0)
+		goto fail_idr;
+	user_key = retval;
+
+	list_add(&item->owner_list, &file_priv->obj_list);
+	mutex_unlock(&dev->struct_mutex);
+
+	mem->offset = dev_priv->vram_offset + (offset << MALI_MM_ALIGN_SHIFT);
+	mem->free = user_key;
+	mem->size = mem->size << MALI_MM_ALIGN_SHIFT;
+
+	return 0;
+
+fail_idr:
+	drm_mm_remove_node(&item->mm_node);
+fail_alloc:
+	kfree(item);
+	mutex_unlock(&dev->struct_mutex);
+
+	mem->offset = 0;
+	mem->size = 0;
+	mem->free = 0;
+
+	DRM_DEBUG("alloc %d, size = %ld, offset = %ld\n", pool, mem->size, mem->offset);
 
 	return retval;
 }
@@ -124,20 +120,30 @@ static int mali_drm_free(struct drm_device *dev, void *data, struct drm_file *fi
 {
 	drm_mali_private_t *dev_priv = dev->dev_private;
 	drm_mali_mem_t *mem = data;
-	int ret;
-	pr_err( "DRM: %s\n", __func__);
+	struct mali_memblock *obj;
+	printk(KERN_ERR "DRM: %s\n", __func__);
 
 	mutex_lock(&dev->struct_mutex);
-	ret = drm_sman_free_key(&dev_priv->sman, mem->free);
+	obj = idr_find(&dev_priv->object_idr, mem->free);
+	if (obj == NULL) {
+		mutex_unlock(&dev->struct_mutex);
+		return -EINVAL;
+	}
+
+	idr_remove(&dev_priv->object_idr, mem->free);
+	list_del(&obj->owner_list);
+	if (drm_mm_node_allocated(&obj->mm_node))
+		drm_mm_remove_node(&obj->mm_node);
+	kfree(obj);
 	mutex_unlock(&dev->struct_mutex);
 	DRM_DEBUG("free = 0x%lx\n", mem->free);
 
-	return ret;
+	return 0;
 }
 
 static int mali_fb_alloc(struct drm_device *dev, void *data, struct drm_file *file_priv)
 {
-	pr_err( "DRM: %s\n", __func__);
+	printk(KERN_ERR "DRM: %s\n", __func__);
 	return mali_drm_alloc(dev, file_priv, data, VIDEO_TYPE);
 }
 
@@ -145,24 +151,17 @@ static int mali_ioctl_mem_init(struct drm_device *dev, void *data, struct drm_fi
 {
 	drm_mali_private_t *dev_priv = dev->dev_private;
 	drm_mali_mem_t *mem= data;
-	int ret;
 	dev_priv = dev->dev_private;
-	pr_err( "DRM: %s\n", __func__);
+	printk(KERN_ERR "DRM: %s\n", __func__);
 
 	mutex_lock(&dev->struct_mutex);
-	ret = drm_sman_set_range(&dev_priv->sman, MEM_TYPE, 0, mem->size >> MALI_MM_ALIGN_SHIFT);
+	drm_mm_init(&dev_priv->mem_mm, 0, mem->size >> MALI_MM_ALIGN_SHIFT);
 
-	if (ret) {
-		DRM_ERROR("MEM memory manager initialisation error\n");
-		mutex_unlock(&dev->struct_mutex);
-		return ret;
-	}
-
-	//dev_priv->agp_initialized = 1;
-	//dev_priv->agp_offset = agp->offset;
+	dev_priv->mem_initialized = 1;
+	dev_priv->mem_offset = mem->offset;
 	mutex_unlock(&dev->struct_mutex);
 
-	//DRM_DEBUG("offset = %u, size = %u\n", agp->offset, agp->size);
+	DRM_DEBUG("offset = %lu, size = %lu\n", mem->offset, mem->size);
 	return 0;
 }
 
@@ -170,15 +169,14 @@ static int mali_ioctl_mem_alloc(struct drm_device *dev, void *data,
 			       struct drm_file *file_priv)
 {
 
-	pr_err( "DRM: %s\n", __func__);
+	printk(KERN_ERR "DRM: %s\n", __func__);
 	return mali_drm_alloc(dev, file_priv, data, MEM_TYPE);
 }
 
-static drm_local_map_t *mem_reg_init(struct drm_device *dev)
+static drm_local_map_t *mali_reg_init(struct drm_device *dev)
 {
 	struct drm_map_list *entry;
 	drm_local_map_t *map;
-	pr_err( "DRM: %s\n", __func__);
 
 	list_for_each_entry(entry, &dev->maplist, head) {
 		map = entry->map;
@@ -194,23 +192,9 @@ static drm_local_map_t *mem_reg_init(struct drm_device *dev)
 int mali_idle(struct drm_device *dev)
 {
 	drm_mali_private_t *dev_priv = dev->dev_private;
-	uint32_t idle_reg;
-	unsigned long end;
-	int i;
-	pr_err( "DRM: %s\n", __func__);
 
 	if (dev_priv->idle_fault)
 		return 0;
-
-	#if 0
-	if (dev_priv->mmio == NULL) {
-		dev_priv->mmio = mali_reg_init(dev);
-		if (dev_priv->mmio == NULL) {
-			DRM_ERROR("Could not find register map.\n");
-			return 0;
-		}
-	}
-	#endif
 
 	return 0;
 }
@@ -219,45 +203,67 @@ int mali_idle(struct drm_device *dev)
 void mali_lastclose(struct drm_device *dev)
 {
 	drm_mali_private_t *dev_priv = dev->dev_private;
+	printk(KERN_ERR "DRM: %s\n", __func__);
 
 	if (!dev_priv) return;
 
 	mutex_lock(&dev->struct_mutex);
-	drm_sman_cleanup(&dev_priv->sman);
-	dev_priv->vram_initialized = 0;
+	if (dev_priv->vram_initialized) {
+		drm_mm_takedown(&dev_priv->vram_mm);
+		dev_priv->vram_initialized = 0;
+	}
+	if (dev_priv->mem_initialized) {
+		drm_mm_takedown(&dev_priv->mem_mm);
+		dev_priv->mem_initialized = 0;
+	}
 	dev_priv->mmio = NULL;
 	mutex_unlock(&dev->struct_mutex);
 }
 
-void mali_reclaim_buffers_locked(struct drm_device * dev, struct drm_file *file_priv)
+void mali_reclaim_buffers_locked(struct drm_device * dev, struct drm_file *file)
 {
-	drm_mali_private_t *dev_priv = dev->dev_private;
-	pr_err( "DRM: %s\n", __func__);
+	struct mali_file_private *file_priv = file->driver_priv;
+	struct mali_memblock *entry, *next;
+	printk(KERN_ERR "DRM: %s\n", __func__);
+
+	if (!(file->minor->master && file->master->lock.hw_lock))
+		return;
+
+printk(KERN_ERR "DRM: %s A\n", __func__);
+	drm_idlelock_take(&file->master->lock);
 
 	mutex_lock(&dev->struct_mutex);
-	if (drm_sman_owner_clean(&dev_priv->sman, (unsigned long)file_priv)) 
-	{
+	if (list_empty(&file_priv->obj_list)) {
 		mutex_unlock(&dev->struct_mutex);
+		drm_idlelock_release(&file->master->lock);
 		return;
 	}
 
-	if (dev->driver->dma_quiescent) 
-	{
-		dev->driver->dma_quiescent(dev);
+printk(KERN_ERR "DRM: %s B\n", __func__);
+	mali_idle(dev);
+
+	list_for_each_entry_safe(entry, next, &file_priv->obj_list,
+				 owner_list) {
+		list_del(&entry->owner_list);
+		if (drm_mm_node_allocated(&entry->mm_node))
+			drm_mm_remove_node(&entry->mm_node);
+		kfree(entry);
 	}
 
-	drm_sman_owner_cleanup(&dev_priv->sman, (unsigned long)file_priv);
 	mutex_unlock(&dev->struct_mutex);
+
+	drm_idlelock_release(&file->master->lock);
+
 	return;
 }
 
-struct drm_ioctl_desc mali_ioctls[] = {
+const struct drm_ioctl_desc mali_ioctls[] = {
 	DRM_IOCTL_DEF(DRM_MALI_FB_ALLOC, mali_fb_alloc, DRM_AUTH),
 	DRM_IOCTL_DEF(DRM_MALI_FB_FREE, mali_drm_free, DRM_AUTH),
 	DRM_IOCTL_DEF(DRM_MALI_MEM_INIT, mali_ioctl_mem_init, DRM_AUTH | DRM_MASTER | DRM_ROOT_ONLY),
 	DRM_IOCTL_DEF(DRM_MALI_MEM_ALLOC, mali_ioctl_mem_alloc, DRM_AUTH),
 	DRM_IOCTL_DEF(DRM_MALI_MEM_FREE, mali_drm_free, DRM_AUTH),
-	DRM_IOCTL_DEF(DRM_MALI_FB_INIT, mali_fb_init, DRM_AUTH | DRM_MASTER | DRM_ROOT_ONLY),
+	DRM_IOCTL_DEF(DRM_MALI_FB_INIT, mali_fb_init, DRM_AUTH | DRM_MASTER | DRM_ROOT_ONLY)
 };
 
 int mali_max_ioctl = DRM_ARRAY_SIZE(mali_ioctls);
